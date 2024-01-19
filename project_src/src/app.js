1869cfc6d9370a95f3b4ee2efd1699dfbb7b5e74
LiaKostova <123201316+LiaKostova@users.noreply.github.com>
2024-01-19 15:31:49 +0200

    Creating page middlewares

--- project_src/src/app.js
+++ project_src/src/app.js
@@ -1,4 +1,48 @@
 import {html, render} from '../node_modules/lit-html/lit-html.js';
 import page from '../node_modules/page/page.mjs';
+import { getUserData } from './utils.js';
 
-console.log("Testing")
+let headerTemplate = () => html`
+  <div class="logo-container">
+            <img src="resources/logo.jpg" alt="Logo" class="logo">
+        </div>
+
+        <nav>
+            <ul class="navigation" role="list">
+                <li class="navigation-li"><a href="/">Начало</a></li>
+                <li class="navigation-li"><a href="/catalog">Настолни игри</a></li>
+                <li class="navigation-li"><a href="/ranking">Класации</a></li>
+                <li class="navigation-li"><a href="/about">За нас</a></li>
+                <li class="navigation-li"><a href="/login">Вход</a></li>
+                <li class="navigation-li"><a href="/register">Регистрация</a></li>
+                <li class="navigation-li"><a href="/logout">Изход</a></li>
+
+            </ul>
+        </nav>
+`
+
+function decorateCtx(ctx, next){
+    ctx.render = function(content){
+        render(content, document.querySelector('main'));
+    }
+    next();
+}
+
+function sessionUserUpdate(ctx, next){
+    let user = getUserData();
+    if(user){
+        ctx.user = user;
+    };
+    next();
+}
+
+function updateHeader(ctx, next){
+    render(headerTemplate(ctx.user), document.querySelector('header'));
+    next();
+}
+
+page(decorateCtx);
+page(sessionUserUpdate);
+page(updateHeader);
+
+page.start();
---
+++ project_src/src/views/login.js
@@ -0,0 +1,7 @@
+import {html} from '../../node_modules/lit-html/lit-html.js';
+
+const loginTemplate = () => html``;
+
+export function showLogin(){
+    
+}
